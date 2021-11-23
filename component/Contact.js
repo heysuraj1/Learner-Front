123eeed9d39c9f26ded5357308a55db0fe336768
Suraj Singh <kumarniteshoffice786@gmail.com>
2021-11-22 20:06:48 -0800

    working contact form

--- component/Contact.js
+++ component/Contact.js
@@ -1,16 +1,56 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 function Contact() {
-    return (
-        <div className="container">
-            <div >
-                <h3 className='henta'>Contact Us Now</h3>
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+
+
+  const handleSubmit = (e) => { 
+    e.preventDefault()
+    console.log('Sending')
+  let data = {
+      name,
+      email,
+      message
+    }
+  fetch('/api/contact', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then((res) => {
+      console.log('Response received')
+      if (res.status === 200) {
+        console.log('Response succeeded!')
+        setSubmitted(true)
+        setName('')
+        setEmail('')
+        
+      }
+    })
+    window.alert('Messge Sent Succesfully')
+  }
+  
+  
+  return (
+    <div className="container">
+      <div>
+        <h3 className="henta">Contact Us Now</h3>
         <form>
           <div className="mb-3">
             <label htmlFor="exampleInputEmail2" className="form-label">
               Full Name
             </label>
             <input
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
               type="text"
               className="form-control"
               id="exampleInputEmail2"
@@ -21,6 +61,9 @@ function Contact() {
               Email address
             </label>
             <input
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
               type="email"
               className="form-control"
               id="exampleInputEmail1"
@@ -35,18 +78,27 @@ function Contact() {
               Your Message
             </label>
             <textarea
+              onChange={(e) => {
+                setMessage(e.target.value);
+              }}
               className="form-control"
               id="exampleFormControlTextarea1"
               rows="7"
             ></textarea>
           </div>
-          <button type="submit" className="btn btn-primary">
+          <button
+            onClick={(e) => {
+              handleSubmit(e);
+            }}
+            type="submit"
+            className="btn btn-primary"
+          >
             Submit
           </button>
         </form>
       </div>
-        </div>
-    )
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
---
+++ pages/api/contact.js
@@ -0,0 +1,29 @@
+export default function (req, res) {
+    require('dotenv').config()
+    
+    let nodemailer = require('nodemailer')
+    const transporter = nodemailer.createTransport({
+      port: 465,
+      host: "smtp.gmail.com",
+      auth: {
+        user: 'kumarniteshoffice786@gmail.com',
+        pass: 'Suraj@786',
+      },
+      secure: true,
+    })
+    const mailData = {
+      from: 'kumarniteshoffice786@gmail.com',
+      to: 'kumarniteshoffice786@gmail.com',
+      subject: `Message From ${req.body.name}`,
+      text: req.body.message + " | Sent from: " + req.body.email,
+      html: `<div>${req.body.message}</div><p>Sent from:
+      ${req.body.email}</p>`
+    }
+    transporter.sendMail(mailData, function (err, info) {
+      if(err)
+        console.log(err)
+      else
+        console.log(info)
+    })
+    res.status(200)
+  }
