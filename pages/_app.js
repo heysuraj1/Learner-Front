031a58a0469733b0d3c31480f72f06dacf7d0546
Suraj Singh <kumarniteshoffice786@gmail.com>
2021-11-25 20:45:52 -0800

    Final Changes

--- pages/_app.js
+++ pages/_app.js
@@ -20,8 +20,7 @@ function MyApp({ Component, pageProps }) {
     <title>LearnerBoy your tech guide</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"/>
     {/* <link href="https://unpkg.com/nprogress@0.2.0/nprogress.css" rel="stylesheet"/> */}
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3220161601867596"
-     crossOrigin="anonymous"></script>
+
     </Head>
     <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></Script>
 
