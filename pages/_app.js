504432f06539c5022a31dab3e0855c994e22efe2
Suraj Singh <kumarniteshoffice786@gmail.com>
2021-11-25 21:16:49 -0800

    Final Changes

--- pages/_app.js
+++ pages/_app.js
@@ -21,6 +21,7 @@ function MyApp({ Component, pageProps }) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"/>
     {/* <link href="https://unpkg.com/nprogress@0.2.0/nprogress.css" rel="stylesheet"/> */}
 
+
     </Head>
     <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></Script>
 
